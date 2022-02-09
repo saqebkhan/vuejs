5d11f2b84d92d25f1dc19522ca8bbd6717afe6d3
Saqeb Khan <86913676+saqebkhan@users.noreply.github.com>
2022-02-09 18:52:46 +0530

    removed table from todo

--- exs's/src/todoList/TodoList.vue
+++ exs's/src/todoList/TodoList.vue
@@ -4,10 +4,6 @@
         <ol>
             <li v-for="(todo, index) in todos" :key="todo"><button @click="del(index)">x</button>{{todo}}</li>
         </ol>
-        <input type="number" v-model="num">
-        <ul v-if="num">
-            <li v-for="item in 10" :key="item" >{{num}} x {{item}} = {{num*item}}</li>
-        </ul>
   </div>
 </template>
 
@@ -17,7 +13,6 @@ export default {
         return {
         input:"",
         todos: [],
-        num:  ''
         }
     },
     methods : {
