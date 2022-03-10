a0fe0b48d7c069ed74cd3f770c20d33f538a9df7
saqebkhan <saqebk619@gmail.com>
2022-03-10 21:28:03 +0530

    saved and pushed

--- my-app/src/views/AboutView.vue
+++ my-app/src/views/AboutView.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quibusdam, ex id nostrum aspernatur, sed magnam porro maxime voluptatum dolores fuga sequi asperiores omnis assumenda eaque minus molestias, itaque fugiat debitis dicta modi perspiciatis? Et repudiandae iste quibusdam obcaecati laborum modi quod quis, exercitationem eos? Facere tenetur error numquam veniam!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sunt placeat illum nostrum repellat neque voluptate quae soluta, molestias impedit! Error eius delectus quo soluta temporibus rerum repellendus sed sint, odit debitis praesentium inventore. At, quaerat. Sed iusto illo minus!</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis repellat aspernatur velit quas consectetur, praesentium commodi quidem aliquam est quos unde odio necessitatibus earum ipsa repudiandae incidunt, magni laborum rem!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nihil optio cumque? Atque dolorem quo provident ea non saepe doloribus.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, optio!</p>
   </div>
 </template>
---
+++ my-app/src/views/Todo.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <!-- <h3 v-if="checked.length != items.length" class="pa-4">
+      Left Todos, {{ items.length - checked.length }} Out Of {{ items.length }}
+    </h3>
+    <h3 v-if="checked.length == items.length" class="pa-4 h">All Done</h3> -->
+    <!-- <h2>checked {{checked}}</h2> -->
+    <h2>items {{ items }}</h2>
+    <v-list>
+      <v-subheader>List Todo</v-subheader>
+      <div class="head">
+        <v-text-field
+          label="Add Todo"
+          :rules="rules"
+          hide-details="auto"
+          v-model="text"
+          @keyup.enter="adder"
+          class="inp"
+          :color="color"
+        ></v-text-field
+        ><v-btn class="mx-2 buttn" fab dark color="indigo" @click="adder">
+          <v-icon dark> mdi-plus </v-icon>
+        </v-btn>
+      </div>
+      <p class="err">{{ te }}</p>
+      <v-list-item v-for="item in items" :key="item.id" class="list">
+        <template v-slot:default>
+          <v-list-item-action>
+            <v-checkbox :input-value="item.check"></v-checkbox>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{
+              item.task
+            }}</v-list-item-title> </v-list-item-content
+          ><v-icon @click="del(item.id)">mdi-delete</v-icon>
+        </template>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          task: "Eating",
+          check: true,
+        },
+      ],
+      text: "",
+      color: "",
+      te: "",
+      heading: "",
+      done: "",
+    };
+  },
+  methods: {
+    adder() {
+      if (this.text.trim().length == 0) {
+        (this.color = "red"), (this.te = "Please Enter The Todo");
+      } else {
+        this.items.push({
+          task: this.text,
+          id: Math.random().toString(),
+          check: false,
+        });
+        this.text = "";
+        (this.color = ""), (this.te = "");
+      }
+    },
+    del(id) {
+      let filteredItems = this.items.filter((ele) => {
+        return ele.id !== id;
+      });
+      this.items = filteredItems;
+      let filteredChecks = this.checks.filter((ele) => {
+        return ele !== id;
+      });
+      this.checks = filteredChecks;
+    },
+  },
+};
+</script>
+<style scoped>
+.inp {
+  margin: 10px;
+}
+.list {
+  width: 300px;
+}
+.head {
+  display: flex;
+  align-items: center;
+  width: 300px;
+}
+.buttn {
+  width: 40px;
+  height: 40px;
+  margin-top: 12px;
+}
+.err {
+  margin-left: 10px;
+  color: red;
+}
+.h {
+  color: green;
+}
+</style>
